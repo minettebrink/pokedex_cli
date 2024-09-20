a9eb0cb0ce463c24367e8d621fe13fab0dbd0b22
minettebrink <mimmi.brink@gmail.com>
2024-09-20 09:39:22 +0200

    api added

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"time"
+
+	"github.com/minettebrink/pokedex_cli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
